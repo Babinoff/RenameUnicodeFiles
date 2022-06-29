330e6a3a16a5b34f90bc7724531ce850aa9cbc3f
babinoff <alex.babinoff.cheat@gmail.com>
2022-06-29 09:17:26 +0300

    non-unicode files fix

--- RenameUnicodeFilesPython/rename_unicode_files.py
+++ RenameUnicodeFilesPython/rename_unicode_files.py
@@ -13,16 +13,24 @@ tree = os.walk(open_folder)
 
 for address, dirs, files in tree:
 	for file in files:
+		print("")
+		print("File check: {}".format(file))
 		file_path = address + "\\" + file
 		clean_file = ""
+		unicode_test = False
 		for symbl in file:
 			if symbl.isprintable():
 				clean_file += symbl
-		print(clean_file)
-		new_file_path = address + "\\" + clean_file
-		if os.path.exists(new_file_path):
-			while os.path.exists(new_file_path):
-				new_file_path = new_file_path.replace(".", "_.")
-				time.sleep(1)
-				print(new_file_path)
-		os.rename(file_path, new_file_path)
+			else:
+				unicode_test = True
+		if unicode_test:
+			print("Unicode found")
+			# print(clean_file)
+			new_file_path = address + "\\" + clean_file
+			if os.path.exists(new_file_path):
+				while os.path.exists(new_file_path):
+					new_file_path = new_file_path.replace(".", "_.")
+					time.sleep(1)
+					# print(new_file_path)
+			print("Clean new file name: {}".format(new_file_path))
+			os.rename(file_path, new_file_path)
